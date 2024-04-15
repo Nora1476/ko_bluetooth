ba035d0357c2b2b2181c1e4172888ee24b274a34
Bonghui(Nora) <nada70979@gmail.com>
2024-04-16 00:01:24 +0900

    240415

--- app/src/main/java/com/example/ko_bluetooth/MainActivity.kt
+++ app/src/main/java/com/example/ko_bluetooth/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.ko_bluetooth
 
+import android.Manifest
+
 import android.bluetooth.BluetoothAdapter
 import android.os.Bundle
 import android.content.Context
+import android.content.pm.PackageManager
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.widget.Toast
 
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_ENABLE_BLUETOOTH = 1
+    private val REQUEST_ACCESS_LOCATION = 2
 
     //액티비티 생명주기 중 하나로, 액티비티가 생성될 때 호출.
     // 이 메소드 내에서 UI를 설정하고, 초기화ㄹ 작업을 수행
@@ -27,6 +34,8 @@ class MainActivity : AppCompatActivity() {
         if (isNetworkAvailable(this)) {
             Toast.makeText(this, "네트워크 연결됨", Toast.LENGTH_LONG).show()
             checkBluetoothState()
+            searchBluetoothDevices()
+
         } else {
             Toast.makeText(this, "네트워크 연결되지 않음", Toast.LENGTH_LONG).show()
         }
@@ -65,5 +74,45 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "이 기기는 블루투스를 지원하지 않습니다.", Toast.LENGTH_LONG).show()
         }
     }
+
+    private fun searchBluetoothDevices() {
+        // 위치 권한을 확인하고 요청합니다.
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_ACCESS_LOCATION)
+        } else {
+            // 권한이 있는 경우 Bluetooth 기기를 검색합니다.
+            startBluetoothDeviceDiscovery()
+            Toast.makeText(this, "기기를 검색합니다.", Toast.LENGTH_LONG).show()
+
+        }
+    }
+
+    private fun startBluetoothDeviceDiscovery() {
+        val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+        if (bluetoothAdapter != null) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.BLUETOOTH_SCAN), REQUEST_ACCESS_LOCATION)
+            } else {
+                bluetoothAdapter.startDiscovery()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_ACCESS_LOCATION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 위치 권한이 허용된 경우 Bluetooth 기기 검색을 시작합니다.
+                    startBluetoothDeviceDiscovery()
+                } else {
+                    // 위치 권한이 거부된 경우 사용자에게 메시지를 표시합니다.
+                    Toast.makeText(this, "위치 권한이 필요합니다.", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+
 
 }
