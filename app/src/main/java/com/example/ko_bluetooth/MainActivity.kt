30f1256ea24e92dca0c029cd84a0c5a8b57b957a
Bonghui(Nora) <nada70979@gmail.com>
2024-04-16 14:42:55 +0900

    240416

--- app/src/main/java/com/example/ko_bluetooth/MainActivity.kt
+++ app/src/main/java/com/example/ko_bluetooth/MainActivity.kt
@@ -23,9 +23,12 @@ import androidx.core.view.WindowInsetsCompat
 
 
 class MainActivity : AppCompatActivity() {
-    private val REQUEST_ENABLE_BLUETOOTH = 1
-    private val REQUEST_ACCESS_LOCATION = 2
+    companion object {
+        private const val REQUEST_ENABLE_BLUETOOTH = 1
+        private const val REQUEST_ACCESS_LOCATION = 2
+    }
 
+    private lateinit var bluetoothAdapter: BluetoothAdapter
 
     //액티비티 생명주기 중 하나로, 액티비티가 생성될 때 호출.
     // 이 메소드 내에서 UI를 설정하고, 초기화 작업을 수행
@@ -35,106 +38,98 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         enableEdgeToEdge()
 
-        // 네트워크 연결 상태 확인
-        if (isNetworkAvailable(this)) {
-            Toast.makeText(this, "네트워크 연결됨", Toast.LENGTH_LONG).show()
-
-        } else {
-            Toast.makeText(this, "네트워크 연결되지 않음", Toast.LENGTH_LONG).show()
-        }
-
-        // 블루투스 상태 확인 및 기기 검색
-        if (isBluetoothSupported()) {
-            checkBluetoothState()
-            searchBluetoothDevices()
-        } else {
-            Toast.makeText(this, "이 기기는 블루투스를 지원하지 않습니다.", Toast.LENGTH_LONG).show()
-        }
-
         //윈도우 인셋을 적절하게 처리하고 뷰의 패딩을 설정하여 시스템 UI와의 겹침을 방지
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setupBluetoothAdapter() //이 어뎁터를 사용하여 블루투스기능 제어
+        checkNetworkAndBluetooth()  //네트워크 및 블루투스 검사
+
     }//onCreate
 
-    //네트워크 연결확인 함수
-    fun isNetworkAvailable(context: Context): Boolean {
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val network = connectivityManager.activeNetwork ?: return false
-        val actNw = connectivityManager.getNetworkCapabilities(network) ?: return false
-        return when {
-            actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-            actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-            actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-            else -> false
+
+    private fun setupBluetoothAdapter() {
+        val manager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothAdapter = manager.adapter
+    }
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun checkNetworkAndBluetooth() {
+        if (isNetworkAvailable()) {
+            showToast("네트워크 연결됨")
+        } else {
+            showToast("네트워크 연결되지 않음")
+        }
+
+        if (isBluetoothSupported()) {
+            checkBluetoothState()
+            requestPermissionsIfNeeded()
+        } else {
+            showToast("이 기기는 블루투스를 지원하지 않습니다.")
         }
     }
-
-    //기기가 블루투스를 지원하는지 여부를 확인
-    private fun isBluetoothSupported(): Boolean {
-        return getBluetoothAdapter() != null
-    }
-    //블루투스 서비스를 관리하는 BluetoothManager 객체를 통해 BluetoothAdapter를 가져오는 것
-    private fun getBluetoothAdapter(): BluetoothAdapter? {
-        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as? BluetoothManager
-        return bluetoothManager?.adapter
+    private fun isNetworkAvailable(): Boolean {
+        val manager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = manager.activeNetwork ?: return false
+        val actNw = manager.getNetworkCapabilities(network) ?: return false
+        return actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
     }
 
 
-    //블루투스 상태확인
+    //기기가 블루투스를 지원 여부 확인
+    private fun isBluetoothSupported() = bluetoothAdapter != null
+    //블루투스 활성화 여부를 확인
     private fun checkBluetoothState() {
-        val bluetoothAdapter = getBluetoothAdapter()
-        if (bluetoothAdapter != null && bluetoothAdapter.isEnabled) {
-            Toast.makeText(this, "블루투스가 활성화되어 있습니다.", Toast.LENGTH_LONG).show()
+        if (bluetoothAdapter.isEnabled) {
+            showToast("블루투스가 활성화되어 있습니다.")
         } else {
-            Toast.makeText(this, "블루투스가 비활성화되어 있습니다.", Toast.LENGTH_LONG).show()
+            showToast("블루투스가 비활성화되어 있습니다.")
         }
     }
 
-    //블루투스 위치권한 확인(물리적 위치정보 유추)
+    //위치 및 블루투스 스캔 권한 요청
     @RequiresApi(Build.VERSION_CODES.S)
-    private fun searchBluetoothDevices() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_ACCESS_LOCATION)
+    private fun requestPermissionsIfNeeded() {
+        requestPermission(Manifest.permission.ACCESS_FINE_LOCATION, REQUEST_ACCESS_LOCATION)
+        requestPermission(Manifest.permission.BLUETOOTH_SCAN, REQUEST_ENABLE_BLUETOOTH)
+    }
+
+    //권한이 부여된 경우 위치 권한에 따라 블루투스 디바이스 검색을 시작
+    private fun requestPermission(permission: String, requestCode: Int) {
+        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
         } else {
-            startBluetoothDeviceDiscovery()
+            if (requestCode == REQUEST_ACCESS_LOCATION) startBluetoothDeviceDiscovery()
         }
     }
 
-    //블루투스 검색권한 확인
-    @RequiresApi(Build.VERSION_CODES.S)
+    //블루투스 스캔 권한이 있는 경우 블루투스 검색
     private fun startBluetoothDeviceDiscovery() {
-        val bluetoothAdapter = getBluetoothAdapter()
-        if (bluetoothAdapter != null && ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.BLUETOOTH_SCAN), REQUEST_ENABLE_BLUETOOTH)
-        } else {
-            bluetoothAdapter?.startDiscovery()
-            Toast.makeText(this, "기기를 검색합니다.", Toast.LENGTH_LONG).show()
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.BLUETOOTH_SCAN
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        if (bluetoothAdapter.startDiscovery()) {
+            showToast("기기를 검색합니다.")
         }
     }
 
-    //블루투스 기기 검색
-    @RequiresApi(Build.VERSION_CODES.S)
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            REQUEST_ACCESS_LOCATION -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    startBluetoothDeviceDiscovery()
-                } else {
-                    Toast.makeText(this, "위치 권한이 필요합니다.", Toast.LENGTH_LONG).show()
-                }
-            }
-            REQUEST_ENABLE_BLUETOOTH -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    startBluetoothDeviceDiscovery()
-                } else {
-                    Toast.makeText(this, "블루투스 검색 권한이 필요합니다.", Toast.LENGTH_LONG).show()
-                }
-            }
-        }
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 
 }
